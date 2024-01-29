db1f2fc1b101ef32ad9602e9d15cb60933897104
Higor <92187145+HigorCris21@users.noreply.github.com>
2024-01-29 17:21:35 -0300

    Ajustando Delagate da TableView

---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Higor  Lo Castro on 29/01/24.
+//  Copyright © 2024 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
