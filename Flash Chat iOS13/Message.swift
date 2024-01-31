494e133f23b2117c565a9f8994f454f39640b485
Higor <92187145+HigorCris21@users.noreply.github.com>
2024-01-31 18:20:14 -0300

    Ajustando comentários

--- Flash Chat iOS13/Message.swift
+++ Flash Chat iOS13/Message.swift
@@ -1,10 +1,3 @@
-//
-//  Message.swift
-//  Flash Chat iOS13
-//
-//  Created by Higor  Lo Castro on 29/01/24.
-//  Copyright © 2024 Angela Yu. All rights reserved.
-//
 
 import Foundation
 
--- Flash Chat iOS13/Views/MessageCell.swift
+++ Flash Chat iOS13/Views/MessageCell.swift
@@ -1,10 +1,4 @@
-//
-//  MessageCell.swift
-//  Flash Chat iOS13
-//
-//  Created by Higor  Lo Castro on 29/01/24.
-//  Copyright © 2024 Angela Yu. All rights reserved.
-//
+
 
 import UIKit
 
