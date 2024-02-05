333c52a619df42443cb8369f1291bbb177c83d5f
Higor <92187145+HigorCris21@users.noreply.github.com>
2024-02-05 18:48:34 -0300

    Ajustando teclado

--- Flash Chat iOS13/AppDelegate.swift
+++ Flash Chat iOS13/AppDelegate.swift
@@ -3,6 +3,7 @@
 import UIKit
 import FirebaseCore
 import Firebase
+import IQKeyboardManagerSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -11,10 +12,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         FirebaseApp.configure()
-        
         let db = Firestore.firestore()
-        
-        print(db)
+  
+        IQKeyboardManager.shared.enable = true
+        IQKeyboardManager.shared.enableAutoToolbar = false
+        IQKeyboardManager.shared.resignOnTouchOutside =  true
+    
         
         return true
     }
